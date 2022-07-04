4351e229c30afbe7c054950f511bd4401850cbc0
Camila Delgado <camiladelgadoperez@gmail.com>
2022-07-04 17:01:37 +0000

    My first commit

--- src/code.py
+++ src/code.py
@@ -2,23 +2,17 @@ import os
 import tweepy
 import requests
 import pandas as pd
-
-consumer_key="UVBYt9C3yVLGd3vQDSgLJnMH6"
-consumer_secret="NgN4liInanqCTKeYrjoLZ7XqrVcpYhKHFo8P5anXydF0xBTMyI"
-access_token="1537937527910191109-SVI7UDUhdXj2Js3Mi1gkmZfW50CsZo"
-access_token_secret="XlSTq8lUSsGp7mKjvAtwwZtZUWVyjXEEFOnVyUCH2To56"
-bearer_token="AAAAAAAAAAAAAAAAAAAAACOGdwEAAAAAKg68UvFvZIRu6qGAKtKDbPlQwZ0%3DDQRYbNYYguBjEBj3Sj743c8gO0G9NH7x11YY7aF7Hj0HnX4h7e"
-
 from dotenv import load_dotenv
-load_dotenv()                    
 
-import os 
+load_dotenv()              
 
 consumer_key = os.environ.get('CONSUMER_KEY')
 consumer_secret = os.environ.get('CONSUMER_SECRET')
 access_token = os.environ.get('ACCESS_TOKEN')
 access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
 bearer_token = os.environ.get('BEARER_TOKEN')
+
+print(consumer_key)
 
 client = tweepy.Client( bearer_token=bearer_token, 
                         consumer_key=consumer_key, 
